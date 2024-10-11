4aaacdc7bb5b0a8757fa40eab6e8978041cf508e
Bruna Barbieri <bruh_barbieri@hotmail.com>
2024-10-10 22:02:16 -0300

    atualizando projeto

---
+++ first-spring-app/src/main/java/com/bruna/first_spring_app/HelloWordController.java
@@ -0,0 +1,14 @@
+package com.bruna.first_spring_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWordController {
+    @GetMapping
+    public String helloWorld() {
+        return "Hello Word!";
+    }
+}
